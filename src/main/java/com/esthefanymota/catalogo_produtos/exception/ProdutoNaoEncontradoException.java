554343dff9a55f0c4d76a3f7e7a93280d8f57341
Esthefany Mota <mottavitoria0601@gmail.com>
2026-01-26 16:47:59 -0300

    Cria exceção ProdutoNaoEncontradoException

--- src/main/java/com/esthefanymota/catalogo_produtos/exception/ProdutoNaoEncontradoException.java
+++ src/main/java/com/esthefanymota/catalogo_produtos/exception/ProdutoNaoEncontradoException.java
@@ -1,4 +1,12 @@
 package com.esthefanymota.catalogo_produtos.exception;
 
-public class ProdutoNaoEncontradoException{
+import java.io.Serial;
+
+public class ProdutoNaoEncontradoException  extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ProdutoNaoEncontradoException(String msg) {
+        super(msg);
+    }
 }
