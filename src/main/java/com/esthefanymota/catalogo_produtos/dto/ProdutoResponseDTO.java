383017e941f15e546178ce3d0979a09aba5d57dd
Esthefany Mota <mottavitoria0601@gmail.com>
2026-01-26 17:30:22 -0300

    Adiciona atributos a classe ResponseDTO

--- src/main/java/com/esthefanymota/catalogo_produtos/dto/ProdutoResponseDTO.java
+++ src/main/java/com/esthefanymota/catalogo_produtos/dto/ProdutoResponseDTO.java
@@ -1,4 +1,59 @@
 package com.esthefanymota.catalogo_produtos.dto;
 
-public class ProdutoResponseDTO {
+import com.esthefanymota.catalogo_produtos.entity.ProdutoEntity;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class ProdutoResponseDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nome;
+    private BigDecimal preco;
+    private String imagemUrl;
+
+    public ProdutoResponseDTO(){}
+
+    public ProdutoResponseDTO(ProdutoEntity object){
+        id = object.getId();
+        nome = object.getNome();
+        preco = object.getPreco();
+        imagemUrl = object.getImagemUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getImagemUrl() {
+        return imagemUrl;
+    }
+
+    public void setImagemUrl(String imagemUrl) {
+        this.imagemUrl = imagemUrl;
+    }
+
 }
