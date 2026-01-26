78928d70c1860b8783ec3e321d79d8d33033cad1
Esthefany Mota <mottavitoria0601@gmail.com>
2026-01-26 17:26:44 -0300

    Adiciona atributos á classe RequestDTO

--- src/main/java/com/esthefanymota/catalogo_produtos/dto/ProdutoRequestDTO.java
+++ src/main/java/com/esthefanymota/catalogo_produtos/dto/ProdutoRequestDTO.java
@@ -1,4 +1,78 @@
 package com.esthefanymota.catalogo_produtos.dto;
 
-public class ProdutoRequestDTO {
+import com.esthefanymota.catalogo_produtos.entity.ProdutoEntity;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class ProdutoRequestDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private Boolean ativo = true;
+    private String imagemUrl;
+
+    public ProdutoRequestDTO(){}
+
+    public ProdutoRequestDTO(ProdutoEntity object){
+        id = object.getId();
+        nome = object.getNome();
+        descricao = object.getImagemUrl();
+        preco = object.getPreco();
+        ativo = object.getAtivo();
+        imagemUrl = object.getImagemUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getImagemUrl() {
+        return imagemUrl;
+    }
+
+    public void setImagemUrl(String imagemUrl) {
+        this.imagemUrl = imagemUrl;
+    }
 }
--- src/main/java/com/esthefanymota/catalogo_produtos/repository/ProdutoRepository.java
+++ src/main/java/com/esthefanymota/catalogo_produtos/repository/ProdutoRepository.java
@@ -2,7 +2,12 @@ package com.esthefanymota.catalogo_produtos.repository;
 
 import com.esthefanymota.catalogo_produtos.entity.ProdutoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProdutoRepository extends JpaRepository <ProdutoEntity, Long>{
 
+    List<ProdutoEntity> findByAtivoTrue();
 }
